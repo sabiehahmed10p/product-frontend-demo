6726e756dad85ec49d14a60611fec4b5a74fae76
Sabieh Ahmed <sabieh.ahmed@gmail.com>
2022-12-28 16:38:39 +0500

    Fix styling

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,62 +1,55 @@
 import type { ReactElement } from 'react'
+import { Link } from 'react-router-dom'
 
-export default function LoginForm(): ReactElement  {
+export default function LoginForm(): ReactElement {
 	return (
-		<div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-			<div className="w-full p-6 m-auto bg-white rounded-md lg:max-w-xl">
-				<h1 className="text-3xl font-semibold text-center text-black">
+		<div className='relative flex min-h-screen flex-col justify-center overflow-hidden'>
+			<div className='m-auto w-full rounded-md bg-white p-6 lg:max-w-xl'>
+				<h1 className='text-center text-3xl font-semibold text-black'>
 					Sign in
 				</h1>
-				<form className="mt-6">
-					<div className="mb-2">
+				<form className='mt-6'>
+					<div className='mb-2'>
 						<label
-							for="email"
-							className="block text-sm font-semibold text-gray-800"
+							for='email'
+							className='block text-sm font-semibold text-gray-800'
 						>
 							Email Address
 						</label>
 						<input
-							type="email"
-							className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+							type='email'
+							className='mt-2 block w-full rounded-md border bg-white px-4 py-2 text-purple-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
 						/>
 					</div>
-					<div className="mb-2">
+					<div className='mb-2'>
 						<label
-							for="password"
-							className="block text-sm font-semibold text-gray-800"
+							for='password'
+							className='block text-sm font-semibold text-gray-800'
 						>
 							Password
 						</label>
 						<input
-							type="password"
-							className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+							type='password'
+							className='mt-2 block w-full rounded-md border bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
 						/>
 					</div>
-					<div className="mt-6">
-						<button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-green-700 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
+					<div className='mt-6'>
+						<button className='w-full transform rounded-md bg-green-700 px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-green-600 focus:bg-green-600 focus:outline-none'>
 							Sign In
 						</button>
 					</div>
-					<a
-						href="#"
-						className="text-xs text-gray-600 hover:underline"
-					>
+					<Link to='/' className='text-xs text-gray-600 hover:underline'>
 						Forget Password?
-					</a>
-
+					</Link>
 				</form>
-
-				<p className="mt-8 text-xs font-light text-center text-gray-700">
-					{" "}
-					Don't have an account?{" "}
-					<a
-						href="#"
-						className="font-medium text-blue-600 hover:underline"
-					>
-						Sign up
-					</a>
+				<p className='mt-8 text-center text-xs font-light text-gray-700'>
+					{' '}
+					Don't have an account?{' '}
+					<Link to='/' className='font-medium text-blue-600 hover:underline'>
+						Sign Up
+					</Link>
 				</p>
 			</div>
 		</div>
-	);
+	)
 }
--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit'
 export const store = configureStore({
 	reducer: {
 		//add reducers here..
-	},
+	}
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
