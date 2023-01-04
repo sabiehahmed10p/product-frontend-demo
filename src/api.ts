ceb782895e73b3fc9536a485183df9bd11fbbd70
Sabieh Ahmed <sabieh.ahmed@gmail.com>
2023-01-04 17:04:25 +0500

    Fix env variable name

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const BASE_URL = `${
 	process.env.NODE_ENV === 'development'
 		? window.location.origin
-		: import.meta.env.VITE_ACTUALS_API_URL
+		: import.meta.env.VITE_API_URL
 }/api`
 
 const Api = axios.create({
